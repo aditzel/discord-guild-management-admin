89e225956f0f4ee9d5e127b730e93d994001ce9a
Allan Ditzel <allan@allanditzel.com>
2018-02-04 00:30:15 -0800

    Added fix for transitive depdency causing problems with spring boot snapshot.

--- src/main/java/com/allanditzel/os/discord/bot/guildmanagement/admin/controller/web/admin/AdminIndexController.java
+++ src/main/java/com/allanditzel/os/discord/bot/guildmanagement/admin/controller/web/admin/AdminIndexController.java
@@ -3,15 +3,12 @@ package com.allanditzel.os.discord.bot.guildmanagement.admin.controller.web.admi
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
 
 @Controller
 @RequestMapping("/admin")
@@ -21,9 +18,6 @@ public class AdminIndexController {
 
     @Autowired
     private OAuth2AuthorizedClientService authorizedClientService;
-
-    @Autowired
-    private UserDetailsService userDetailsService;
 
     @Autowired
     public AdminIndexController() {
